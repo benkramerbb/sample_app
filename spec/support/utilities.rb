cc4d8914af1c9477c3a419280796013baf5059b7
benkramer <benmkramer@gmail.com>
2013-01-15 21:34:58 -0500

    Chapter 9 sections 1 and 2

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -6,3 +6,20 @@ def full_title(page_title)
     "#{base_title} | #{page_title}"
   end
 end
+
+def sign_in(user)
+  visit signin_path
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  cookies[:remember_token] = user.remember_token
+end
+
+def edit_user(user, name, email)
+  visit edit_user_path(user)
+  fill_in "Name",             with: name
+  fill_in "Email",            with: email
+  fill_in "Password",         with: user.password
+  fill_in "Confirm Password", with: user.password
+  click_button "Save changes"
+end
