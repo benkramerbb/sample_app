c0af06fa0fca0760838ba63fad6df436d797121e
benkramer <benmkramer@gmail.com>
2013-01-16 20:43:10 -0500

    Finish user edit, update, index, and destroy actions +++ added ajax destroy which will flash success for 3 seconds and fadeout deleted user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   before_filter :signed_in_user,  only: [:index, :edit, :update]
   before_filter :correct_user,    only: [:edit, :update]
+  before_filter :admin_user,      only: :destroy
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
 
   def show
@@ -40,6 +41,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    respond_to do |format|
+      format.html do
+        flash[:success] = "User destroyed."
+        redirect_to users_url
+      end
+      format.js
+    end
+
+  end
+
   private
     def signed_in_user
       store_location
@@ -49,5 +63,9 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to root_path unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to root_path unless current_user.admin?
     end
 end
